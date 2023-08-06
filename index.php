6b6051d645af96fe94646a2e0a2ef9513babd403
rishav5490 <rishavrajput2018@gmail.com>
2023-08-06 23:52:37 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+include("includes/header.php")
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4 col-lg-4">
+                            <div class="first-page">
+                                <div class="row">
+                                    <img src="images/tanu1.jpeg" alt="images">
+                                    <div>
+                                        <div class="row">
+                                            <h6>
+                                                <marquee>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Nitika Rajput
+                                                    !!!!!!!!!!!!!!!!!!!!!!!!!!!!</marquee>
+                                            </h6>
+                                            <h2>NITIKA KUMARI</h2>
+                                            <h3>NITIKA RAJPUT</h3>
+                                            <h4>01TANUTHAKUR__</h4>
+                                            <h5>NITIKARAJPUT2018@GMAIL.COM</h5>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-4 col-lg-4">
+                            <div class="first-page">
+                                <div class="row">
+                                    <img src="images/rishav1.jpg" alt="images">
+                                    <div>
+                                        <div class="row">
+                                            <h6>
+                                                <marquee>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Rishav Rajput
+                                                    !!!!!!!!!!!!!!!!!!!!!!!!!!!!</marquee>
+                                            </h6>
+                                            <h2>RISHAV KUMAR</h2>
+                                            <h3>RISHAV RAJPUT</h3>
+                                            <h4>01RISHAVTHAKUR__</h4>
+                                            <h5>RISHAVRAJPUT2018@GMAIL.COM</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-4 col-lg-4">
+                            <div class="first-page">
+                                <div class="row">
+                                    <img src="images/tanu4.jpeg" alt="images">
+                                    <div>
+                                        <div class=" row">
+                                            <h6>
+                                                <marquee>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Nitika Rajput
+                                                    !!!!!!!!!!!!!!!!!!!!!!!!!!!!</marquee>
+                                            </h6>
+                                            <h2>NITIKA KUMARI</h2>
+                                            <h3>NITIKA RAJPUT</h3>
+                                            <h4>01TANUTHAKUR__</h4>
+                                            <h5>NITIKARAJPUT2018@GMAIL.COM</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</body>
+
+</html>
